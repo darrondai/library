2bde826c5d380ce7bfbdb0319ce893b67c7ef281
Darron Dai <daidarron@gmail.com>
2024-12-09 14:30:09 -0800

    Add starter code

---
+++ main.js
@@ -0,0 +1,17 @@
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+
+Book.prototype.info = function () {
+  return `${this.title} by ${this.author}, ${this.pages} pages, ${
+    this.read ? "read" : "not read yet"
+  }`;
+};
+
+theHobbit = new Book("The Hobbit", "J.R.R Tolkien", 295, true);
+console.log(theHobbit.info());
