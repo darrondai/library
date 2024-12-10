04cc7b65413057999fa704f50d4dc43c60205132
Darron Dai <daidarron@gmail.com>
2024-12-09 19:36:03 -0800

    Rewrite book instantiations, add 3 more books

--- main.js
+++ main.js
@@ -24,14 +24,24 @@ function createBookElement(book) {
   return bookView;
 }
 
-theHobbit = new Book("The Hobbit", "J.R.R Tolkien", 295, true);
-addBookToLibrary(myLibrary, theHobbit);
+// instantiate books and add to lib list
+addBookToLibrary(myLibrary, new Book("The Hobbit", "J.R.R Tolkien", 295, true));
 
-today = new Book("Today", "Q", 23, false);
-addBookToLibrary(myLibrary, today);
+addBookToLibrary(myLibrary, new Book("Today", "Q", 23, false));
 
-passport = new Book("Passport", "Redacted", 15, false);
-addBookToLibrary(myLibrary, passport);
+addBookToLibrary(myLibrary, new Book("Passport", "Redacted", 15, false));
+
+addBookToLibrary(
+  myLibrary,
+  new Book("A Court of Thorns and Roses", "Sarah J. Maas", 448, false)
+);
+
+addBookToLibrary(myLibrary, new Book("Funny Story", "Emily Henry", 400, false));
+
+addBookToLibrary(
+  myLibrary,
+  new Book("Crying in H Mart: A Memoir", "Michelle Zauner", 256, false)
+);
 
 // query DOM to get library HTML element
 const library_list = document.querySelector(".library-list");
