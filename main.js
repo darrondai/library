1aae80ef90f7ebe30251d080d5597198ce3254fd
Darron Dai <daidarron@gmail.com>
2024-12-09 16:19:36 -0800

    Add function for creating book html elems

--- main.js
+++ main.js
@@ -13,5 +13,31 @@ Book.prototype.info = function () {
   }`;
 };
 
+function addBookToLibrary(library, book) {
+  library.push(book);
+}
+
+function createBookElement(book) {
+  let bookView = document.createElement("li");
+  bookView.classList.add("book");
+  bookView.textContent = book.info();
+  return bookView;
+}
+
 theHobbit = new Book("The Hobbit", "J.R.R Tolkien", 295, true);
-console.log(theHobbit.info());
+addBookToLibrary(myLibrary, theHobbit);
+
+today = new Book("Today", "Q", 23, false);
+addBookToLibrary(myLibrary, today);
+
+passport = new Book("Passport", "Redacted", 15, false);
+addBookToLibrary(myLibrary, passport);
+
+// query DOM to get library HTML element
+const library_list = document.querySelector(".library-list");
+
+// make an element for each book in the library
+for (let book of myLibrary) {
+  let bookView = createBookElement(book);
+  library_list.appendChild(bookView);
+}
